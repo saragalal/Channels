a92227df839b0ea6f47db40d8121248278d5bb2a
sara <sara.galal@ibtikar.net.sa>
2020-03-30 23:12:52 +0200

    add cartfile and update carthage

---
+++ Channels/Scene/SpalshScene/SpalshViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SpalshViewController.swift
+//  Channels
+//
+//  Created by sara.galal on 3/30/20.
+//  Copyright © 2020 Ibtikar. All rights reserved.
+//
+
+import UIKit
+
+class SpalshViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Channels/Scene/SpalshScene/SplashCoordinator.swift
@@ -0,0 +1,9 @@
+//
+//  SplashCoordinator.swift
+//  Channels
+//
+//  Created by sara.galal on 3/30/20.
+//  Copyright © 2020 Ibtikar. All rights reserved.
+//
+
+import Foundation
